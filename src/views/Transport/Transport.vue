335ce29bd8b05f27c82aa8b563e330508d723456
Abdo <abdo.maxmud@gmail.com>
2023-04-16 21:20:37 +0500

    replace order to component

--- src/views/Transport/Transport.vue
+++ src/views/Transport/Transport.vue
@@ -69,12 +69,7 @@
               <div class="col">
                 <button
                   class="btn btn-sm btn-block btn-outline-info"
-                  @click="
-                    order = item;
-                    getLoan(item);
-                  "
-                  data-toggle="modal"
-                  data-target="#order"
+                  @click="$refs.orderModal.start(item.id)"
                 >
                   <i class="fa fa-info"></i>
                 </button>
@@ -102,136 +97,16 @@
     </div>
   </div>
 
-  <div class="modal fade" id="order">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5>Buyurtma</h5>
-        </div>
-        <div class="modal-body">
-          <div
-            class="responsive"
-            style="min-height: 70vh; zoom: 90%"
-            v-if="order"
-          >
-            <div class="row my-1" v-if="order && order.income && order.balance">
-              <div class="col-md-4">
-                Buyurtma summasi
-                <br />
-                {{
-                  Intl.NumberFormat().format(order.balance.total_price) +
-                  " so'm"
-                }}
-              </div>
-              <div class="col-md-4">
-                To'lov summa
-                <br />
-                <span v-for="(i, index) in order.income" :key="i">
-                  {{
-                    i.Incomes.comment +
-                    ": " +
-                    Intl.NumberFormat().format(i.Incomes.money) +
-                    " so'm" +
-                    (index !== order.income.length - 1 ? ", " : "")
-                  }}
-                  <br />
-                </span>
-              </div>
-              <div class="col-md-4">
-                Nasiya summa
-                <br />
-                {{
-                  Intl.NumberFormat().format(
-                    order.balance.total_price -
-                      (order.income[0].Incomes.money +
-                        (order.income[1] ? order.income[1].Incomes.money : 0)) -
-                      order.discount >
-                      0
-                      ? order.balance.total_price -
-                          (order.income[0].Incomes.money +
-                            (order.income[1]
-                              ? order.income[1].Incomes.money
-                              : 0)) -
-                          order.discount
-                      : 0
-                  ) + " so'm"
-                }}
-              </div>
-            </div>
-            <div class="table-responsive my-1" v-if="order.trades">
-              <table class="table table-sm table-hover">
-                <thead>
-                  <tr>
-                    <th>Mahsulot</th>
-                    <th>Narx</th>
-                    <th>Chegirma</th>
-                    <th>Miqdor</th>
-                    <th>Qaytarilgan</th>
-                    <th>Summa</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="i in order.trades" :key="i">
-                    <td>
-                      {{ i.Categories.name + " - " + i.Products.articul }}
-                    </td>
-                    <td>
-                      {{ Intl.NumberFormat().format(i.Trades.price) + " so'm" }}
-                    </td>
-                    <td>
-                      {{
-                        Intl.NumberFormat().format(i.Trades.discount) + " so'm"
-                      }}
-                    </td>
-                    <td>
-                      {{ i.sum_quantity + " dona" }}
-                    </td>
-                    <td>
-                      {{
-                        (order.returned_products.find((item) => {
-                          return i.Trades.code == item.Returned_products.code;
-                        })
-                          ? order.returned_products.find((item) => {
-                              return (
-                                i.Trades.code == item.Returned_products.code
-                              );
-                            }).sum_quantity
-                          : "0") + " dona"
-                      }}
-                    </td>
-                    <td>
-                      {{
-                        Intl.NumberFormat().format(
-                          (i.Trades.price - i.Trades.discount) * i.sum_quantity
-                        ) + " so'm"
-                      }}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button
-            class="btn btn-outline-danger"
-            data-dismiss="modal"
-            close-order-modal
-          >
-            <i class="far fa-circle-xmark" />
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <orderModal ref="orderModal" />
 </template>
 
 <script>
 import * as api from "@/components/Api/Api.js";
 import Pagination from "@/components/Pagination/Pagination.vue";
+import orderModal from "@/components/order/orderModal.vue";
 export default {
   name: "Transport",
-  components: { Pagination },
+  components: { Pagination, orderModal },
   data() {
     return {
       currency: Intl.NumberFormat(),
@@ -269,46 +144,6 @@ export default {
         api.success().then(() => {
           this.getOrders(0, 25);
         });
-      });
-    },
-    getLoan(order) {
-      this.order.loan = null;
-      this.order.income = [];
-      this.order.balance = null;
-      this.order.trades = [];
-      this.order.returned_products = [];
-      if (order.customer) {
-        api.loan(order.customer.id).then((Response) => {
-          order.loan = Response.data;
-          this.getIncome(order);
-        });
-      } else {
-        this.getIncome(order);
-      }
-    },
-    getIncome(order) {
-      api.incomes(order.id, "order", 0, 100).then((Response) => {
-        this.order.income = Response.data.data;
-        this.getBalance(order);
-      });
-    },
-    getBalance(order) {
-      api.tradeBalance(order.id).then((Response) => {
-        this.order.balance = Response.data;
-        this.getTrades(order, 0, 100);
-      });
-    },
-    getTrades(order, page, limit) {
-      api.trades(order.id, page, limit).then((Response) => {
-        this.page_2 = Response.data.current_page;
-        this.pages_2 = Response.data.pages;
-        this.order.trades = Response.data.data;
-        this.getReturnedProducts(order, page, limit);
-      });
-    },
-    getReturnedProducts(order, page, limit) {
-      api.returnedProducts(order.id, page, limit).then((Response) => {
-        this.order.returned_products = Response.data.data;
       });
     },
   },
