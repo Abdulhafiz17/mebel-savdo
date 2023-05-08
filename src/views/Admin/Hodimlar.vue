25126b5804eab13df434888a51375210f076ce6c
Abdo <abdo.maxmud@gmail.com>
2023-05-08 11:54:49 +0500

    remove add user from cashier

--- src/views/Admin/Hodimlar.vue
+++ src/views/Admin/Hodimlar.vue
@@ -5,6 +5,7 @@
     <div class="col-md-4 my-1"></div>
     <div class="col-md-4">
       <button
+        v-if="role == 'admin'"
         class="btn btn-sm btn-block btn-outline-secondary"
         data-toggle="modal"
         data-target="#hodimQoshish"
@@ -495,7 +496,7 @@ export default {
   created() {
     this.get(0, 100);
     this.getBranches();
-    this.getWarehouses();
+    if (this.role == "admin") this.getWarehouses();
   },
   methods: {
     format(number) {
