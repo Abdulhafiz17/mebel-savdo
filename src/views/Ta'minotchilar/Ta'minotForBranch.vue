3387225dc0c261c7ed177c282cc3edbc8dc54922
Abdo <abdo.maxmud@gmail.com>
2023-05-30 18:08:14 +0500

    add gruzchik id

--- src/views/Ta'minotchilar/Ta'minotForBranch.vue
+++ src/views/Ta'minotchilar/Ta'minotForBranch.vue
@@ -267,7 +267,8 @@ export default {
           this.from_time,
           this.to_time,
           1,
-          1
+          1,
+          0
         )
         .then((Response) => {
           this.page = Response.data.current_page;
