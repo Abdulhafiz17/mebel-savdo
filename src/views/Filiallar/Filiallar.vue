17c1699b0f2fd6cbbb1d1ee88accce4c5a12204f
Abdo <abdo.maxmud@gmail.com>
2023-06-04 18:15:31 +0500

    hide loans router

--- src/views/Filiallar/Filiallar.vue
+++ src/views/Filiallar/Filiallar.vue
@@ -133,7 +133,7 @@
                   <i class="fa fa-cash-register" />
                 </router-link>
               </div>
-              <div class="col my-1" v-if="role !== 'admin'">
+              <div class="col my-1" v-if="false">
                 <router-link
                   :to="`/nasiyalar?branch_id=${item.id}`"
                   class="btn btn-sm btn-block btn-outline-primary"
